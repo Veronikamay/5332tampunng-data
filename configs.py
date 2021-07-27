1269d78daf15aac71fc83c6fd7a87578b7604440
panggil aku shifa <87982800+Veronikamay@users.noreply.github.com>
2021-07-28 05:55:32 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,15 +3,15 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
-	API_HASH = os.environ.get("API_HASH")
+	API_ID = int(os.environ.get("7541940"))
+	API_HASH = os.environ.get("c3844ac69bde05d7ff6c634d05f78e54")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = int(os.environ.get("UPDATES_CHANNEL"))
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	DB_CHANNEL = int(os.environ.get("-1001551633328"))
+	BOT_OWNER = int(os.environ.get("1862482411"))
+	DATABASE_URL = os.environ.get("-1001551633328")
+	UPDATES_CHANNEL = int(os.environ.get("-1001494029572"))
+	LOG_CHANNEL = int(os.environ.get("-1001551633328"))
 	ABOUT_BOT_TEXT = f"""
 **𝕀ℕ𝔽𝕆 𝔹𝕆𝕋**
 bot ini ᵐᵉʳᵘᵖᵃᵏᵃⁿ ˡᵃʸᵃⁿᵃⁿ ᵖᵉⁿʸⁱᵐᵖᵃⁿᵃⁿ ᵃʷᵃⁿ ᵗᵉˡᵉᵍʳᵃᵐ ᵈⁱᵐᵃⁿᵃ ᵏᵃᵐᵘ ᵇⁱˢᵃ ᵐᵉⁿʸⁱᵐᵖᵃⁿ ᵈᵃᵗᵃ ᵐᵃᵘᵖᵘⁿ ᶠⁱˡᵉ (ᵛⁱᵈᵉᵒ, ᵃᵘᵈⁱᵒ, ᵈᵒᵏᵘᵐᵉⁿ, ᵍᵃᵐᵇᵃʳ, ᵈˡˡ) ᵗᵃⁿᵖᵃ ʰᵃʳᵘˢ ᵗᵃᵏᵘᵗ ᵏᵉʰⁱˡᵃⁿᵍᵃⁿ ᶠⁱˡᵉ ᵗᵉʳˢᵉᵇᵘᵗ. ᵈⁱˢᵃᵐᵖⁱⁿᵍ ⁱᵗᵘ, ᵏᵃᵐᵘ ʲᵘᵍᵃ ᵇⁱˢᵃ ᵐᵉⁿᵍᵃᵏˢᵉˢ ᶠⁱˡᵉⁿʸᵃ ˢᵉʷᵃᵏᵗᵘ-ʷᵃᵏᵗᵘ ᵇⁱˡᵃ ᵈⁱᵖᵉˡᵘᵏᵃⁿ ᵈᵉⁿᵍᵃⁿ ᶜᵃʳᵃ ᵐᵉᵐᵇᵘᵏᵃ ᶠⁱˡᵉ ᵐᵉˡᵃˡᵘⁱ ˡⁱⁿᵏ ˡᵃˡᵘ ᵐᵉⁿᵈᵒʷⁿˡᵒᵃᵈⁿʸᵃ.\n\n**Link akan tersedia bila kamu telah subscribe channels dan telah mengirim data/file** !!
